83124ea6cea72647e3f12fc57993b2c301657572
Gabriel Abdalla Cavalcante <gabriel.silva@serpro.gov.br>
2014-02-04 09:08:00 -0200

    Arquivo de distribuição setup.py atualizado.
    O arquivo setup.py possui informações do projeto para possível
    envio ao PyPi.

--- setup.py
+++ setup.py
@@ -1,4 +1,22 @@
 from distutils.core import setup
-import py2exe
+try:
+    import py2exe
+except ImportError:
+    py2exe = None
 
-setup(console=["logs.py"])
+setup(name='log-dump',
+      version='0.1',
+      scripts=['log_dump.py', ],
+      author='Gabriel Abdalla Cavalcante',
+      author_email='gabriel.cavalcante88@gmail.com',
+      url='https://github.com/gcavalcante8808',
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache License',
+        'Operating System :: Win32 :: Windows',
+        'Programming Language :: Python',
+        'Topic :: System :: Systems Administration',
+      ]
+)
