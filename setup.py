a828a96d7ef1379ba92c814bf05baea32910f530
Gabriel Abdalla Cavalcante <gabriel.silva@serpro.gov.br>
2014-02-04 10:15:41 -0200

    Corrected Classifiers values at setup.py.

--- setup.py
+++ setup.py
@@ -9,14 +9,19 @@ setup(name='log-dump',
       scripts=['log_dump.py', ],
       author='Gabriel Abdalla Cavalcante',
       author_email='gabriel.cavalcante88@gmail.com',
+      description = ("""Log-Dump is a Python script to dump
+		      and generate all Windows Logon Errors,
+		 primary the 4625 and 4771 events on
+		Windows 2008 / 2012 Servers and
+		529 on Windows 2003 Servers."""),
       url='https://github.com/gcavalcante8808',
       classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
         'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: Apache License',
-        'Operating System :: Win32 :: Windows',
-        'Programming Language :: Python',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 2 :: Only',
         'Topic :: System :: Systems Administration',
       ]
 )
