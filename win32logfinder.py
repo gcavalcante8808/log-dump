716f96af1d057c62d368e45a2e93aecb60a164fa
gcavalcante8808 <gabriel.cavalcante88@gmail.com>
2015-04-30 16:38:18 -0300

    Updated COP info.

--- win32logfinder.py
+++ win32logfinder.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # __author__ = '01388863189'
 #
-# Done by Gabriel Abdalla Cavalcante Silva at Receita Federal do Brasil,
+# Done by Gabriel Abdalla Cavalcante Silva
 #
 # Licensed under the Apache License, Version 2.0, that can be viewed at:
 # http://www.apache.org/licenses/LICENSE-2.0
