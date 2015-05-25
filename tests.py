a5e493ba9bf0abf8b80daa86972002177f483f45
gcavalcante8808 <gabriel.cavalcante88@gmail.com>
2015-05-25 14:59:12 -0300

    Added Stubs for unittests.

---
+++ tests.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# __author__ = 'Gabriel Abdalla Cavalcante Silva'
+#
+# Done by Gabriel Abdalla Cavalcante Silva
+#
+# Licensed under the Apache License, Version 2.0, that can be viewed at:
+# http://www.apache.org/licenses/LICENSE-2.0
+
